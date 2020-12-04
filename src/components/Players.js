c4c546192801f105f16c0d128f608240d6f89f81
Barbara Peric <b.peric10@gmail.com>
2020-12-04 17:40:35 +0100

    FEAT: added Player component to Players UI

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,12 +1,57 @@
 import React from 'react'
 import usePlayers from '../hooks/usePlayers'
+import { 
+  Route, 
+  Switch,
+  Link, 
+  useLocation, 
+  useParams, 
+  useRouteMatch 
+} from "react-router-dom";
+import slug from 'slug'
+
+const Player = ({ players }) => {
+  const { playerId }= useParams()
+
+  const player = players.find(player => slug(player.name) === playerId)
+  console.log('player', player)
+  return (
+    <div className="panel fade-enter-done">
+      <img src={player.avatar} alt={`${player.name}'s avatar`} className="avatar"/>
+      <h1 className="medium-header">{player.name}</h1>
+      <h3 className="header">{player.number}</h3>
+      <div className="row">
+        <ul className="info-list" style={{marginRight: 80}}>
+          <li>Team</li>
+          <div>
+            <Link to="/">
+              {player.teamId}
+            </Link>
+          </div>
+          <li>Position<div>{player.position}</div></li>
+          <li>PPG<div>{player.ppg}</div></li>
+        </ul>
+        <ul className='info-list'>
+          <li>APG<div>{player.apg}</div></li>
+          <li>SPG<div>{player.spg}</div></li>
+          <li>RPG<div>{player.rpg}</div></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
 
 const Players = () => {
+
+  //const location = useLocation()
+  const { url } = useRouteMatch()
 
   const {
     response: players,
     loading
   } = usePlayers()
+  
+  //console.log(usePlayers())
 
   if (loading === true) {
     return <p>LOADING</p>
@@ -19,15 +64,29 @@ const Players = () => {
         <ul className="sidebar-list">
           {players.map(player => (
             <li style={{fontWeight: 'normal'}} key={player.name}>
-              <a href="/">
+              <Link
+                to={{
+                pathname: `${url}/${slug(player.name)}`,
+                //search: location.search
+                }}
+              >
                 {player.name}
-              </a>
+              </Link>
             </li>
           ))}
         </ul>
       </div >
-      <div className="sidebar-instruction">Select a player</div>
+      
+      <Switch>
+        <Route path={`${url}/:playerId`}>
+            <Player players={players}/>
+        </Route>
+        <Route path="*">
+          <div className="sidebar-instruction">Select a player</div>
+        </Route>
+      </Switch>
     </div>
+    
   )
 }
 
